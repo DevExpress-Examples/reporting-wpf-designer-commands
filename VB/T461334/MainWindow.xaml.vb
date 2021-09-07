d2b93b214b93a78185b48ba3c6f013ce771c4bca
DevExpressExampleBot <fakeEmail>
2021-09-07 19:21:20 +0300

    Source auto update [skip ci]

--- VB/T461334/MainWindow.xaml.vb
+++ VB/T461334/MainWindow.xaml.vb
@@ -3,29 +3,29 @@ Imports System.Windows
 Imports DevExpress.Xpf.Reports.UserDesigner
 
 Namespace T461334
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-            designer.Commands = New CustomDesignerCommands()
-            AddHandler Loaded, AddressOf MainWindow_Loaded
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+			designer.Commands = New CustomDesignerCommands()
+			AddHandler Me.Loaded, AddressOf MainWindow_Loaded
+		End Sub
 
-        Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            designer.OpenDocument(New SampleReport())
-        End Sub
-    End Class
+		Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			designer.OpenDocument(New SampleReport())
+		End Sub
+	End Class
 
-    Public Class CustomDesignerCommands
-        Inherits ReportDesignerCommands
+	Public Class CustomDesignerCommands
+		Inherits ReportDesignerCommands
 
-        Protected Overrides Sub SaveDocumentAs()
-            Debug.WriteLine("CustomDesignerCommands.SaveDocumentAs")
-            MyBase.SaveDocumentAs()
-        End Sub
-    End Class
+		Protected Overrides Sub SaveDocumentAs()
+			Debug.WriteLine("CustomDesignerCommands.SaveDocumentAs")
+			MyBase.SaveDocumentAs()
+		End Sub
+	End Class
 End Namespace
--- VB/T461334/SampleReport.vb
+++ VB/T461334/SampleReport.vb
@@ -5,12 +5,12 @@ Imports System.ComponentModel
 Imports DevExpress.XtraReports.UI
 
 Namespace T461334
-    Partial Public Class SampleReport
-        Inherits DevExpress.XtraReports.UI.XtraReport
+	Partial Public Class SampleReport
+		Inherits DevExpress.XtraReports.UI.XtraReport
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-    End Class
+	End Class
 End Namespace
