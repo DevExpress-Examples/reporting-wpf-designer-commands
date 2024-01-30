8ca118957f3e2b94ec9f2671e457f4d127d16ea6
DevExpressExampleBot <fakeEmail>
2024-01-30 14:09:17 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -3,29 +3,30 @@ Imports System.Windows
 Imports DevExpress.Xpf.Reports.UserDesigner
 
 Namespace T461334
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			designer.Commands = New CustomDesignerCommands()
-			AddHandler Me.Loaded, AddressOf MainWindow_Loaded
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			designer.OpenDocument(New SampleReport())
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.designer.Commands = New CustomDesignerCommands()
+            AddHandler Loaded, AddressOf Me.MainWindow_Loaded
+        End Sub
 
-	Public Class CustomDesignerCommands
-		Inherits ReportDesignerCommands
+        Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.designer.OpenDocument(New SampleReport())
+        End Sub
+    End Class
 
-		Protected Overrides Sub SaveDocumentAs()
-			Debug.WriteLine("CustomDesignerCommands.SaveDocumentAs")
-			MyBase.SaveDocumentAs()
-		End Sub
-	End Class
+    Public Class CustomDesignerCommands
+        Inherits ReportDesignerCommands
+
+        Protected Overrides Sub SaveDocumentAs()
+            Call Debug.WriteLine("CustomDesignerCommands.SaveDocumentAs")
+            MyBase.SaveDocumentAs()
+        End Sub
+    End Class
 End Namespace
