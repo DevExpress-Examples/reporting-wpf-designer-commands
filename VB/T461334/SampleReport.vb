34150be1259d8244c7557f559f7a672266a186f2
DevExpressExampleBot <fakeEmail>
2024-01-31 06:32:16 +0400

    Source auto update [skip ci]

--- VB/T461334/SampleReport.vb
+++ VB/T461334/SampleReport.vb
@@ -1,16 +1,14 @@
-﻿Imports System
 Imports System.Drawing
-Imports System.Collections
 Imports System.ComponentModel
 Imports DevExpress.XtraReports.UI
 
 Namespace T461334
-	Partial Public Class SampleReport
-		Inherits DevExpress.XtraReports.UI.XtraReport
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class SampleReport
+        Inherits XtraReport
 
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 End Namespace
